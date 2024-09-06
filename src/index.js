b34afbfb823b0c1c5ab337ed4be0b0d1985a87a4
MitchyUvvU <148670288+mitcheltastic@users.noreply.github.com>
2024-09-06 21:23:17 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,18 +9,24 @@ require('./features/tokenCleanup/tokenCleanup');
 const Routes = require("./routes/routes");
 const cors = require('cors');
 
+// Mengatur opsi CORS
 let corsOptions = {
   origin: "http://localhost:3000",
-
 };
+
+// Menggunakan middleware CORS
 app.use(cors(corsOptions));
 app.use(express.json());
 
+// Menangani rute dasar
 app.get('/', (req, res) => {
   res.send('Hello World!');
-})
+});
+
+// Menggunakan rute API
 app.use("/api", Routes);
 
+// Menjalankan server
 app.listen(PORT, () => {
-  console.log(Server running on port ${PORT});
+  console.log(`Server running on port ${PORT}`);
 });
